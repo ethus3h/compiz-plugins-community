c422f3985d8d166bc4b7ffde9e0f870d21a297f8
Eignar17 <Eignar17@gmail.com>
2018-04-12 02:30:35 -0500

    update to http://cgit.compiz.org

--- src/headtracking/facedetect.h
+++ src/headtracking/facedetect.h
@@ -1,2 +1,2 @@
-int headtrack(int *x1, int *y1, int *x2, int *y2, int lissage, int smooth, int delay);
+int headtrack(int *x1, int *y1, int *x2, int *y2, int lissage, int smooth, int delay, int opt_scale);
 void endThread(void);
